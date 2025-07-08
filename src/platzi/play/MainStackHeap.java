3f29c4cd42f45ac124bc3797d530413f72e1ae88
Alejandro Ramirez <alejandroramirez@saimyr.com.co>
2025-07-08 10:40:03 -0500

    Clase 13 - Ciclo de vida de los objetos.

---
+++ src/platzi/play/MainStackHeap.java
@@ -0,0 +1,17 @@
+package platzi.play;
+
+import platzi.play.contenido.Pelicula;
+
+public class MainStackHeap {
+    public static void main(String[] args) {
+        Pelicula reyLeon = new Pelicula("El Rey León", 135, "Animada");
+        Pelicula harryPotter = new Pelicula("Harry Potter", 200, "Fantasía");
+
+        reyLeon = harryPotter;
+
+        reyLeon.titulo = "El Hobbit";
+
+        System.out.println("reyLeon: " + reyLeon.titulo);
+        System.out.println("harryPotter: " + harryPotter.titulo);
+    }
+}
