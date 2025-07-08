7afb41ab1c7719002bb1335a6808dc591d458752
Alejandro Ramirez <alejandroramirez@saimyr.com.co>
2025-07-08 11:15:20 -0500

    Clase 15 - Listas en Java

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -1,18 +1,16 @@
 package platzi.play;
 
 import platzi.play.contenido.Pelicula;
+import platzi.play.plataforma.Plataforma;
 import platzi.play.plataforma.Usuario;
 import platzi.play.util.ScannerUtils;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Scanner;
 
 public class Main {
     public static final String NOMBRE_PLATAFORMA = "PLATZI PLAY 🍿";
     public static final String VERSION = "1.0.0";
 
     public static void main(String[] args) {
+        Plataforma plataforma = new Plataforma(NOMBRE_PLATAFORMA);
         System.out.println(NOMBRE_PLATAFORMA + " v" + VERSION);
 
         String nombre = ScannerUtils.capturarTexto("Nombre del contenido");
@@ -21,8 +19,14 @@ public class Main {
         double calificacion = ScannerUtils.capturarDecimal("Calificacion del contenido");
 
         Pelicula pelicula = new Pelicula(nombre, duracion, genero, calificacion);
+        Pelicula pelicula2 = new Pelicula("F1 The Movie", 220, "Acción");
 
-        System.out.println(pelicula.obtenerFichaTecnica());
+        plataforma.agregar(pelicula);
+        plataforma.agregar(pelicula2);
+        System.out.println("Numero de elementos en la plataforma " + plataforma.getContenido().size());
+        plataforma.eliminar(pelicula2);
+
+        plataforma.mostrarTitulos();
 
         Usuario usuario = new Usuario("Juan", "juan@platzi.com");
         usuario.ver(pelicula);
