3f9b8190060ae286685c7ae9d852e55d0701dbf9
Alejandro Ramirez <alejandroramirez@saimyr.com.co>
2025-07-09 11:11:11 -0500

    Clase 26 - escritura de archivos planos.

--- src/platzi/play/plataforma/Plataforma.java
+++ src/platzi/play/plataforma/Plataforma.java
@@ -4,6 +4,7 @@ import platzi.play.contenido.Genero;
 import platzi.play.contenido.Pelicula;
 import platzi.play.contenido.ResumenContenido;
 import platzi.play.excepcion.PeliculaExistenteException;
+import platzi.play.util.FileUtils;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -29,6 +30,7 @@ public class Plataforma {
             throw new PeliculaExistenteException(elemento.getTitulo());
         }
 
+        FileUtils.escribirContenido(elemento);
         this.contenido.add(elemento);
     }
 
